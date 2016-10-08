74e50399e1590fc29e93726bfd227a6d478b8c83
Sam Rushing <gitsam@rushing.nightmare.com>
2016-10-08 15:39:50 -0700

    writers moved to emit.scm

--- test/test_emit.scm
+++ test/test_emit.scm
@@ -4,20 +4,6 @@
 (include "tdfa.scm")
 (include "emit.scm")
 
-(define (make-writer file)
-  (let ((level 0))
-    (define (write-string s)
-      (write file.fd
-	     (format (repeat level "    ") s "\n"))
-      #u)
-    (define (copy s)
-      (write file.fd s))
-    (define (indent) (set! level (+ level 1)))
-    (define (dedent) (set! level (- level 1)))
-    (define (close-file) (close file.fd))
-    {write=write-string indent=indent dedent=dedent copy=copy close=close-file}
-    ))
-
 (define (t0 r)
   (let ((rx (parse-rx r)))
     (printf "rx: " (rx-repr rx) "\n")
@@ -27,15 +13,10 @@
       (printf (int nstates) " states\n")
       (let ((nfa0 (nfa->map nfa nstates))
 	    (tdfa (nfa->tdfa nfa0)))
-	(dump-flat-dfa tdfa.machine)
-	(printf (int tdfa.nregs) " registers\n")
-	(printf "finals:\n")
-	(for-map f insns tdfa.finals
-	  (printf "  " (int f) ": " (tag-set-repr insns) "\n"))
-	(printf "\n")
-	(let ((ofile (file/open-write "t1.py" #t #o644))
-	      (o (make-writer ofile)))
-	  (emit-python o tdfa)
+	(dump-tdfa tdfa)
+	(let ((o (file-writer "t1.c" "  ")))
+	  ;;(emit-python o tdfa)
+	  (emit-hybrid-c o tdfa)
 	  (o.close)
 	  )
 	))))
@@ -43,11 +24,19 @@
 ;(t0 ".*({a+}|{b+})")
 ;(t0 ".*({a+})")
 ;(t0 ".*{ab|bc}")
-(t0 ".*({BBB\\-BB\\-BBBB}|{BBBB\\-BBBB\\-BBBB\\-BBBB})")
+
+(let ((rx0 ".*({ddd[-]dd[-]dddd}|{dddd[-]dddd[-]dddd[-]dddd})")
+      (rx1 (format (join "[0-9]" (string-split rx0 #\d)))))
+  (printf "rx = " rx1 "\n")
+  (t0 rx1))
+
+;(t0 ".*({ddd\\-dd\\-dddd}|{dddd\\-dddd\\-dddd\\-dddd})")
 
 ;(define scheme-keywords 
 ;  "lambda|define|if|else|cond|and|or|case|let|let\\*|letrec|begin|do|delay|set\\!"
 ;  )
 
 ;(t0 (format ".*{(" scheme-keywords ")=}"))
+;(t0 ".*{[A-Z][0-9]+}")
+;(t0 ".*{(the|if|not|splits|future)=}")
 
