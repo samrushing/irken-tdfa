e0b9a2a7c717d3ed1a624c7880ce4ad4bbea5131
Sam Rushing <gitsam@rushing.nightmare.com>
2016-10-07 20:37:51 -0700

    use list-of-ranges rather than set-of-ranges.
    This roughly doubled the speed of the compiler when dealing with large machines (e.g. 5000 word OR).
    Since the charset/merge operation took two sorted sets and returned a sorted set, the use of the set datatype was unnecessary and really slowed down charset operations, which had to be turned back into lists anyway.

--- charset.scm
+++ charset.scm
@@ -27,9 +27,9 @@
 ;;  that overlap) but I think this is a good compromise.
 
 ;; hi is not inclusive. this simplifies merging.
-(datatype charset
-  (:t (tree int int)) ;; [lo hi)
-  )
+;(datatype charset
+;  (:t (list {lo=int hi=int})) ;; [lo hi)
+;  )
 
 ;; Note: as written, this assumes 8-bit characters.  Supporting wider
 ;;  charsets should be trivial (simply replace the occurrences of "256" below).
@@ -51,28 +51,24 @@
     (maybe:no)      -> (format "<" (zpad 2 (hex ch)) ">")
     ))
 
-(define charset/size
-  (charset:t s)
-  -> (let ((size 0))
-       (for-map lo hi s
-	 (set! size (+ size (- hi lo))))
-       size))
+(define (charset/size s)
+  (let ((size 0))
+    (for-list r s
+      (set! size (+ size (- r.hi r.lo))))
+    size))
 
-(define charset-repr*
-  (charset:t m)
-  -> (let ((ranges '()))
-       (tree/reverse
-	(lambda (lo hi)
-	  (cond ((= hi (+ 1 lo)) 
-		 (PUSH ranges (char-repr lo)))
-		((= hi (+ 2 lo))
-		 (PUSH ranges (char-repr (+ 1 lo)))
-		 (PUSH ranges (char-repr lo)))
-		(else
-		 (PUSH ranges (format (char-repr lo) "-" (char-repr (- hi 1)))))))
-	m)
-       (string-concat ranges)
-       ))
+(define (charset-repr* s)
+  (let ((ranges '()))
+    (for-list r s
+      (cond ((= r.hi (+ 1 r.lo)) 
+	     (PUSH ranges (char-repr r.lo)))
+	    ((= r.hi (+ 2 r.lo))
+	     (PUSH ranges (char-repr (+ 1 r.lo)))
+	     (PUSH ranges (char-repr r.lo)))
+	    (else
+	     (PUSH ranges (format (char-repr r.lo) "-" (char-repr (- r.hi 1)))))))
+    (string-concat ranges)
+    ))
 
 (define (charset-repr s)
   (let ((size (charset/size s)))
@@ -82,66 +78,37 @@
 	  (else
 	   (format "[" (charset-repr* s) "]")))))
 
-(define charset->list
-  (charset:t m)
-  -> (let ((r '()))
-       (tree/reverse
-	(lambda (lo hi)
-	  (PUSH r (:tuple lo hi)))
-	m)
-       r))
-
 (define (charset-repr-raw s)
   (let ((r '()))
-    (for-list x (charset->list s)
-      (match x with
-	(:tuple lo hi)
-	-> (PUSH r (format (zpad 2 (hex lo)) "-" (zpad 2 (hex hi))))))
+    (for-list x s
+      (PUSH r (format (zpad 2 (hex x.lo)) "-" (zpad 2 (hex x.hi)))))
     (format "{" (join "," (reverse r)) "}")))
 
 ;; note: this is not meant for overlap detection, it is
 ;;  for maintaining sets/maps of charsets.
-(define charset<
-  (charset:t a) (charset:t b)
-  -> (let ((g0 (tree/make-generator a))
-	   (g1 (tree/make-generator b)))
-       (let loop ()
-	 (match (g0) (g1) with
-	   (maybe:no)    (maybe:no)    -> #f
-	   (maybe:no)    (maybe:yes _) -> #t
-	   (maybe:yes _) (maybe:no)    -> #f
-	   (maybe:yes (:tuple ak av)) (maybe:yes (:tuple bk bv))
-	   -> (cond ((< ak bk) #t)
-		    ((< bk ak) #f)
-		    ((< av bv) #t)
-		    ((< bv av) #f)
-		    (else (loop)))
-	   ))
-       ))
-
-(define list->charset*
-  ()                    acc -> acc
-  ((:tuple lo hi) . tl) acc -> (list->charset* tl (tree/insert acc < lo hi))
-  )
-
-(define (list->charset l)
-  (charset:t (list->charset* l (tree:empty))))
+(define (charset< a b)
+  (match a b with
+    () ()  -> #f
+    () _   -> #t
+    _ ()   -> #f
+    (ra . tla) (rb . tlb)
+    -> (cond ((< ra.lo rb.lo) #t)
+	     ((< rb.lo ra.lo) #f)
+	     ((< ra.hi rb.hi) #t)
+	     ((< rb.hi ra.hi) #f)
+	     (else (charset< tla tlb)))))
 
 (define (charset/empty)
-  (charset:t (tree:empty)))
+  (list:nil))
 
 (define (charset/single n)
-  (charset:t (tree/make < (n (+ n 1)))))
+  (LIST {lo=n hi=(+ n 1)}))
 
 (define (charset/range lo hi)
   (assert (< lo hi))
-  (charset:t (tree/make < (lo hi))))
+  (LIST {lo=lo hi=hi}))
 
 (define charset/dot (charset/range 0 256))
-
-(define charset->map
-  (charset:t m) -> m
-  )
 
 ;;    -----XXX-----XXXX-----XXX----
 ;; a)  ch
@@ -149,14 +116,13 @@
 ;; c)          ch
 
 ;; XXX this could be smarter
-(define charset/in*
-  (charset:t m) ch
-  -> (let/cc return 
-       (for-map lo hi m
-	 (cond ((< ch lo) (return #f)) ;; a
-	       ((<= hi ch) #f)	       ;; c [note: not a return]
-	       (else (return #t))))    ;; b
-       (return #f)))
+(define (charset/in* s ch)
+  (let/cc return 
+    (for-list r s
+      (cond ((< ch r.lo) (return #f)) ;; a
+	    ((<= r.hi ch) #f)	      ;; c [note: not a return]
+	    (else (return #t))))      ;; b
+    (return #f)))
 
 (define (charset/in set ch)
   (charset/in* set (char->ascii ch)))
@@ -173,42 +139,87 @@
 
 ;; Note: for merging, only a & b imply overlap. so we need only
 ;;   test for c & d.
-(define cmp-range0
-  alo ahi blo bhi
-  -> (cond ((< ahi blo) (cmp:<))	;; case c
-	   ((< bhi alo) (cmp:>))	;; case d
-	   (else (cmp:=))))
+(define (cmp-range0 alo ahi blo bhi)
+  (cond ((< ahi blo) (cmp:<)) ;; case c
+	((< bhi alo) (cmp:>)) ;; case d
+	(else (cmp:=))))
 
 ;; this version is used for merging
-(define cmp-range
-  (:tuple alo ahi) (:tuple blo bhi)
-  -> (cmp-range0 alo ahi blo bhi))
+(define (cmp-range a b)
+  (cmp-range0 a.lo a.hi b.lo b.hi))
 
 ;; this version is used for overlap detection
 ;; [note: the hi end of the range is open, so
 ;;  we need to decrement it to detect overlap]
-(define cmp-range-overlap
-  (:tuple alo ahi) (:tuple blo bhi)
-  -> (cmp-range0 alo (- ahi 1) blo (- bhi 1)))
+
+(define (cmp-range-overlap a b)
+  (cmp-range0 a.lo (- a.hi 1) b.lo (- b.hi 1)))
 
 ;; XXX consider using generators here (especially for unicode)
 
 ;; Note: this is O(#ranges), but I bet it's possible
 ;;  to do better using their sorted nature. [probably by adding
 ;;  matches like this: (ra) (rb . tlb) where ahi < blo
+
 (define (charset/overlap? a b)
-  (let loop ((la (charset->list a))
-	     (lb (charset->list b)))
-    (match la lb with
-      () _ -> #f
-      _ () -> #f
-      (ra . tla) (rb . tlb)
-      -> (match (cmp-range-overlap ra rb) with
-	   (cmp:=) -> #t
-	   (cmp:<) -> (loop tla lb)
-	   (cmp:>) -> (loop la tlb)
-	   )
-      )))
+  (match a b with
+    () _ -> #f
+    _ () -> #f
+    (ra . tla) (rb . tlb)
+    -> (match (cmp-range-overlap ra rb) with
+	 (cmp:=) -> #t
+	 (cmp:<) -> (charset/overlap? tla b)
+	 (cmp:>) -> (charset/overlap? a tlb)
+	 )
+    ))
+
+;; memoization
+
+;; (define memo<
+;;   {a=a0 b=b0} {a=a1 b=b1}
+;;   -> (cond ((< a0 a1) #t)
+;; 	   ((< a1 a0) #f)
+;; 	   (else (< b0 b1))))
+
+;; (define *charsets* (cmap/make charset<))
+;; (define *charset-pairs* (cmap/make memo<))
+;; (define *overlaps* (make-vector 20000 0))
+
+;; (define *calls* 0)
+;; (define *hits* 0)
+
+;; XXX ai/bi should be canonicalized, could cut
+;;   the size of *charset-pairs* in half.
+
+;; (define (charset/overlap? a b)
+;;   (set! *calls* (+ 1 *calls*))
+;;   (let ((ai (cmap/add *charsets* a))
+;; 	(bi (cmap/add *charsets* b))
+;; 	(oi (cmap/add *charset-pairs* {a=ai b=bi})))
+;;     (cond ((= *overlaps*[oi] 0)
+;; 	   (let ((overlap? (charset/overlap?* a b)))
+;; 	     (set! *overlaps*[oi] (if overlap? 2 1))
+;; 	     overlap?))
+;; 	  (else
+;; 	   (set! *hits* (+ 1 *hits*))
+;; 	   (= *overlaps*[oi] 2)))))
+
+;;; generator version
+;; (define (charset/overlap? a b)
+;;   (let ((ga (tree/make-generator (charset->map a)))
+;; 	(gb (tree/make-generator (charset->map b))))
+;;     (let loop ((a (ga))
+;; 	       (b (gb)))
+;;       (match a b with
+;; 	(maybe:no) _ -> #f
+;; 	_ (maybe:no) -> #f
+;; 	(maybe:yes ra) (maybe:yes rb)
+;; 	-> (match (cmp-range-overlap ra rb) with
+;; 	     (cmp:=) -> #t
+;; 	     (cmp:<) -> (loop (ga) b)
+;; 	     (cmp:>) -> (loop a (gb))
+;; 	     )
+;; 	))))
 
 ;; merge two charsets.
 ;; merge both charsets into a single map, then iterate through
@@ -218,36 +229,34 @@
 
 (define (charset/merge a b)
 
-  (define charset/add
-    s (:tuple lo hi) -> (tree/insert s < lo hi)
-    )
+  ;; merge-sort two range lists
+  (define merge
+    () lb -> lb
+    la () -> la
+    (ha . ta) (hb . tb)
+    -> (if (< ha.lo hb.lo)
+	   (list:cons ha (merge ta (list:cons hb tb)))
+	   (list:cons hb (merge (list:cons ha ta) tb))))
 
-  (define range/merge 
-    ;; we know these ranges overlap, return a new merged range.
-    (:tuple alo ahi) (:tuple blo bhi)
-    -> (:tuple (min alo blo) (max ahi bhi))
-    )
+  ;; we know these ranges overlap, return a new merged range.
+  (define (range/merge ra rb)
+    {lo=(min ra.lo rb.lo) hi=(max ra.hi rb.hi)})
 
-  (let ((m0 (charset->map a))
-	(m1 '()))
-    (for-map lo hi (charset->map b)
-      (tree/insert! m0 < lo hi))
-    (set! m1 (charset->list (charset:t m0)))
-    (if (null? m1)
-    	(charset:t (tree:empty))
-    	(let loop ((acc (car m1))
-    		   (l (cdr m1))
-    		   (r (tree/make <)))
+  (let ((m (merge a b)))
+    (if (null? m)
+	(charset/empty)
+    	(let loop ((acc (car m))
+    		   (l (cdr m))
+    		   (r (list:nil)))
 	  (match l with
-	    () -> (charset:t (charset/add r acc))
+	    () -> (reverse (list:cons acc r))
 	    (hd . tl)
 	    -> (match (cmp-range acc hd) with
 	  	 (cmp:=) -> (loop (range/merge acc hd) tl r)
-	  	       _ -> (loop hd tl (charset/add r acc))
-	  	 )
+	  	       _ -> (loop hd tl (list:cons acc r))
+		 )
 	    )
-	  ))
-    ))
+	  ))))
 
 ;;    a-b c-d e-f
 ;; -> \00-a b-c d-e f-\FF
@@ -255,13 +264,13 @@
 ;;   side of a match? [so we can make it a global constant]
 (define (charset/invert s)
   (let loop ((end 0) 
-	     (r (tree:empty))
-	     (ls (charset->list s)))
-    (match ls with
-      ()                    -> (charset:t (tree/insert r < end 256))
-      ((:tuple lo 256))     -> (charset:t (tree/insert r < end lo))
-      ((:tuple 0 hi) . tl)  -> (loop hi r tl)
-      ((:tuple lo hi) . tl) -> (loop hi (tree/insert r < end lo) tl)
+	     (r (list:nil))
+	     (s s))
+    (match s with
+      ()                   -> (reverse (list:cons {lo=end hi=256} r))
+      ({lo=lo hi=256})     -> (reverse (list:cons {lo=end hi=lo} r))
+      ({lo=0 hi=hi} . tl)  -> (loop hi r tl)
+      ({lo=lo hi=hi} . tl) -> (loop hi (list:cons {lo=end hi=lo} r) tl)
       )))
 
 (define (parse-charset s)
@@ -288,4 +297,30 @@
     ;; backslashes? hex-escapes, ']', '[', etc..
     ))
 
+
+;;; thoughts on speeding up the tdfa build:
+;;;   rather than repeatedly compare character sets, how can
+;;;   we memoize it?  One idea, compute the full distinct alphabet
+;;;   (as in hopcroft.scm), and then do a table of S X S.  Replace
+;;;   all nfa.sym with an index into this table.
+;;; we could test this idea without too much work... if we make a 
+;;;   table of all distinct charsets, then we can do simple memoization
+;;;   using a map or 2d vector.
+;;; how would this work?
+;;;   we use a cmap to collect all unique charsets.
+;;;   then we use a map of (a,b) pairs to memoize.
+;;; I think this could be used with many datatypes, since
+;;;   the user only need provide a sort function to feed the
+;;;   cmap.
+;;; ...
+;;; ok that didn't help.
+;;;
+;;; calls 1070010
+;;; hits  1055767
+;;; |charsets| 402
+;;; |pairs| 14243
+;;;
+;;; good hit rate, but it took twice as long to run.
+;;; looks like we could benefit from memoization, but
+;;;   how to do it quickly?
 
